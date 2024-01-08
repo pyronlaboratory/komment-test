787b803a78b2ce8a8566d45c12a715cbe126433d
pyronlaboratory <ronnie94official@gmail.com>
2024-01-08 21:05:18 +0000

    Local bulk commit to trigger pipe A

--- api.ts
+++ api.ts
@@ -1,18 +1,8 @@
 class Api {
-/**
-* @description This function logs the message "API is running live!" to the console.
-* 
-* @returns { void } There is no output returned by this function because it does not
-* contain a return statement. The function simply logs a message to the console using
-* `console.log()`.
-*/
   public static run(): void {
     console.log("API is running live!");
   }
 }
 
 Api.run();
-
-Api.run();
-
 
--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-/**
-* @description The function takes a single argument `port`, and logs the message
-* "App is running live on port: " followed by the value of `port` to the console.
-* 
-* @param { number } port - The `port` input parameter specifies the port number where
-* the app will listen for incoming requests.
-*/
 const app = (port) => {
   console.log("App is running live on port: ", port)
 }
