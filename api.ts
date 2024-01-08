a76e0e4969bc8667031e26f7d26f331386ba3f28
pyronlaboratory <ronnie94official@gmail.com>
2024-01-08 17:27:55 +0000

    Local commit

--- api.ts
+++ api.ts
@@ -1,11 +1,4 @@
 class Api {
-/**
-* @description This function simply logs the message "API is running live!" to the
-* console.
-* 
-* @returns { void } There is no output returned by the function since `console.log`
-* is used and does not return a value.
-*/
   public static run(): void {
     console.log("API is running live!");
   }
--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-/**
-* @description The function app returns undefined and immediately logs "App is running
-* live!" to the console without returning anything or taking any input.
-*/
 const app = () => {
   console.log("App is running live!");
 };
