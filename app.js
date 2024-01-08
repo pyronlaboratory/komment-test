c383fde53147b5fa03e6b394cc864f4f9479089e
pyronlaboratory <ronnie94official@gmail.com>
2024-01-08 17:05:42 +0000

    Local commit

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-/**
-* @description The function defines an empty function `app`, which will not execute
-* any code when called.
-*/
 const app = () => {
   console.log("App is running live!");
 };
