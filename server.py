b302849ec64a65985af0d63bbc7ac00fc5864738
pyronlaboratory <ronnie94official@gmail.com>
2024-01-08 21:51:47 +0000

    Local bulk commit to trigger pipe A with file 1/2/4/5

--- server.py
+++ server.py
@@ -1,13 +1,2 @@
 def server(port):
-  
-  """
-  The function `server` listens for incoming requests on a specific port ( specified
-  as an argument ) and prints a message indicating that the server is listening
-  on that port.
-
-  Args:
-      port (str): The `port` input parameter specifies which port the server should
-          listen on.
-
-  """
   print(f"Server listening on port: {port}")
--- services.js
+++ services.js
@@ -1,11 +1,3 @@
-/**
-* @description The given function is a anonymous self-executing function that logs
-* the message "Services are being queued on port: [port]" to the console whenever
-* it's invoked with an argument (port).
-* 
-* @param { number } port - The `port` input parameter is used to pass a single
-* argument to the function.
-*/
 const services = (port) => {
 
     console.log("Services are being queued on port: ", port)
