dd7f5c83dbb3178b54534a4130e208d1d2b18c80
pyronlaboratory <ronnie94official@gmail.com>
2024-01-09 22:54:14 +0000

    Local commit for file 1 to trigger pipe A

--- api.ts
+++ api.ts
@@ -1,9 +1,5 @@
 class Api {
-/**
-* @description This function logs the message "API is running live!" to the console.
-* 
-* @returns { void } The function does not return any value and does not throw any error.
-*/
+
   public static run(): void {
     console.log("API is running live!");
   }
