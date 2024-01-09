c35ff47f1153cc73adf5f76119f63f4478c47c4a
pyronlaboratory <ronnie94official@gmail.com>
2024-01-09 22:30:54 +0000

    Local bulk commit with files 4/5 for Pipe B

--- server.py
+++ server.py
@@ -1,12 +1,2 @@
 def server(port):
-  
-  """
-  This function declares a server that listens on a specific port (as indicated
-  by the `port` argument passed to the function).
-
-  Args:
-      port (int): The `port` input parameter specifies the port number that the
-          server should listen on.
-
-  """
   print(f"Server listening on port: {port}")
