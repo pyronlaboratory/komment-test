24fe8b38a28890e895a0884abc703d16bc47fb5b
pyronlaboratory <ronnie94official@gmail.com>
2024-01-09 22:25:58 +0000

    Local bulk commit with files 1/2/3 for Pipe A

--- api.ts
+++ api.ts
@@ -1,10 +1,5 @@
 class Api {
-/**
-* @description This function logs the message "API is running live!" to the console.
-* 
-* @returns { void } The output of the function `run()` is `console.log("API is running
-* live!)".
-*/
+
   public static run(): void {
     console.log("API is running live!");
   }
--- auth.js
+++ auth.js
@@ -1,11 +1,3 @@
-/**
-* @description This function takes a single argument `port`, and logs the message
-* "Authentication is redirecting on port: [PORT]" to the console.
-* 
-* @param { number } port - The `port` input parameter is not used at all inside the
-* function `auth`. It is passed to the function but not utilized or accessed within
-* the function body.
-*/
 const auth = (port) => {
 
   console.log("Authentication is redirecting on port: ", port)
