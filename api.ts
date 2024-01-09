1bd906ac09a3e62dbe57328ba990914aeef115fd
Ronnie <ronnie94official@gmail.com>
2024-01-09 15:50:11 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,11 +1,5 @@
 class Api {
-/**
-* @description This function logs the message "API is running live!" to the console.
-* 
-* @returns { void } The output returned by this function is:
-* 
-* "API is running live!"
-*/
+
   public static run(): void {
     console.log("API is running live!");
   }
