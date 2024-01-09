dc30608dc64fff8abdc5a2aefe03aa5b420ef572
pyronlaboratory <ronnie94official@gmail.com>
2024-01-09 22:22:06 +0000

    Local bulk commit with files 5/6 to trigger pipe B

--- server.py
+++ server.py
@@ -1,11 +1,2 @@
 def server(port):
-  """
-  The function `server(port)` prints a message to the console indicating that the
-  server is listening on the specified port.
-
-  Args:
-      port (int): The `port` input parameter specifies the TCP port number that
-          the server should listen on for incoming connections.
-
-  """
   print(f"Server listening on port: {port}")
