6ac2ec2805dee1334c79a6dcdb8524681eb30d8c
pyronlaboratory <ronnie94official@gmail.com>
2024-01-09 22:37:23 +0000

    Local bulk commit with files 1/2/3 for Pipe A

--- api.ts
+++ api.ts
@@ -1,13 +1,11 @@
 class Api {
-/**
-* @description This function logs the message "API is running live!" to the console.
-* 
-* @returns { void } The output of the function is "API is running live!" since the
-* `console.log()` statement within the function will log that message to the console.
-*/
   public static run(): void {
     console.log("API is running live!");
   }
 }
+
+
+
+
 Api.run();
 
--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-/**
-* @description This function takes a single argument `port`, and logs a message to
-* the console indicating that the app is running live on that port.
-* 
-* @param { number } port - The `port` input parameter passed to the `app` function
-* takes a value that determines which port number the app should listen on for
-* incoming requests.
-*/
 const app = (port) => {
   console.log("App is running live on port: ", port)
 }
