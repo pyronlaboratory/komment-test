395b3a0096b8eb8dcebe4d71578eff9e39542630
pyronlaboratory <ronnie94official@gmail.com>
2024-01-09 23:51:03 +0000

    Local commit - reset

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-/**
-* @description The function takes a single argument `port`, and logs the message
-* "App is running live on port: <port>" to the console.
-* 
-* @param { number } port - The `port` input parameter is passed as an argument to
-* the function and it allows the function to specify the port number that the app
-* should run on.
-*/
 const app = (port) => {
   console.log("App is running live on port: ", port)
 }
--- auth.js
+++ auth.js
@@ -1,13 +1,3 @@
-/**
-* @description This function takes a single argument `port`, logs a message to the
-* console indicating that authentication is being redirected on that port (specifically
-* logging "Authentication is redirecting on port: \*port*"), and then does nothing
-* else (i.e., it has no return value or side effects).
-* 
-* @param { number } port - The `port` input parameterin the `auth` function is not
-* used or consumedin any way. It is simply passed to the console.log statement as
-* an argument and appears to be included only for documentation purposes.
-*/
 const auth = (port) => {
   console.log("Authentication is redirecting on port: ", port)
 }
