5d83f9c4e68041b1543235d5b9795ba77205adf6
Ronnie <ronnie94official@gmail.com>
2024-01-10 10:21:45 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,10 +1,4 @@
 class Api {
-/**
-* @description This function simply logs the message "API is live!" to the console.
-* 
-* @returns { void } There is no output returned by this function because `console.log()`
-* statement is used and it does not return anything.
-*/
   public static run(): void {
     console.log("API is live!");
   }
