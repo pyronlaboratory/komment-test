ab3084d1e810f6c3e2f164bb07aa1bf7ed273c3c
pyronlaboratory <ronnie94official@gmail.com>
2024-01-10 17:46:33 +0000

    Local bulk commit

--- api_new.ts
+++ api_new.ts
@@ -1,14 +1,5 @@
-class Api_new {
-/**
-* @description This function logs a message to the console indicating that the API
-* is live and new code changes have been made.
-* 
-* @returns { void } The output of the function `run()` is:
-* 
-* `console.log("API is live (new code changes..)!");`
-* 
-* This will log the string "API is live (new code changes..)!!" to the console.
-*/
+class Api {
+
   public static run(): void {
     console.log("API is live (new code changes..)!");
   }
--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-/**
-* @description This function takes a single argument `port` and logs a message to
-* the console indicating that the app is running live on that port.
-* 
-* @param { number } port - The `port` input parameter specifies the TCP port where
-* the app should listen for incoming requests.
-*/
+
 const app = (port) => {
-  
+
   console.log("App is running live on port: ", port)
 }
