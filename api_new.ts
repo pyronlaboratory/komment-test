1f0c6ed1c2725ffa886f22d01e45fbd6ffb91184
Ronnie <ronnie94official@gmail.com>
2024-01-10 10:24:51 +0000

    Update api_new.ts

--- api_new.ts
+++ api_new.ts
@@ -1,10 +1,4 @@
 class Api_new {
-/**
-* @description This function logs a message to the console indicating that the API
-* is live and that new code changes are present.
-* 
-* @returns { void } The output of the function `run()` is `undefined`.
-*/
   public static run(): void {
     console.log("API is live (new code changes..)!");
   }
