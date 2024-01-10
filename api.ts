ddcc4ad8a9935b96672c02ac4f08ffdc6c67f064
Ronnie <ronnie94official@gmail.com>
2024-01-10 10:19:16 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,11 +1,4 @@
 class Api {
-/**
-* @description This function logs the message "API is live!" to the console.
-* 
-* @returns { void } The function `run()` is attempting to call the `console.log()`
-* method with the string `"API is live!"`. However、`console.log()` is not a function
-* and returns `undefined`.
-*/
   public static run(): void {
     console.log("API is live!");
   }
