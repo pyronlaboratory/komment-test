784e9d9c97ce59d4e98b28962a8157b6cab4b7ed
Ronnie <ronnie94official@gmail.com>
2024-01-12 17:34:19 +0000

    Update api.ts ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////

--- api.ts
+++ api.ts
@@ -1,11 +1,4 @@
 class Api {
-/**
-* @description The provided code is a JavaScript function named `run`, which is
-* defined as a public static method.
-* 
-* @returns { void } There is no output returned by this function because `console.log()`
-* does not return any value. It is a statement that prints a message to the console.
-*/
   public static run(): void {
     console.log("API is live!");
   }
