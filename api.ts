5a8a36aa44d8af37ccbc9fe92d26924d165489af
Ronnie <ronnie94official@gmail.com>
2024-01-12 20:12:24 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,15 +1,3 @@
-const Api {
-/**
-* @description This function logs the message "API is live!" to the console.
-* 
-* @returns { void } The output of the function `run()` is "API is live!" because the
-* function logs that message to the console using `console.log()`.
-*/
-  public static run(): void {
-    console.log("API is live!");
-  }
+function log() {
+  console.warn("0");
 }
-
-Api.run();
-
-
