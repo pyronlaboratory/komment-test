83e2c2b08e22dc685530997ea9ffaf9857f169ca
Ronnie <ronnie94official@gmail.com>
2024-01-15 14:58:01 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,9 +1,4 @@
-/**
-* @description This function does nothing because the `() =>` syntax is an arrow
-* function with no body. It is a null function that will not perform any operations
-* when called.
-*/
-const log() =>  {
-  console.warn("0");
+const log = () =>  {
+  console.dog("0");
 }
 
