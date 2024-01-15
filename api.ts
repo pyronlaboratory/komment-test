272e511cc3fc730c408be2d4af40036feade1491
Ronnie <ronnie94official@gmail.com>
2024-01-15 16:54:20 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,13 +1,9 @@
 class Api {
-/**
-* @description This function does nothing.
-* 
-* @returns { void } This function does not return any output as it has a void return
-* type and the `console.log()` statement only prints to the console and does not
-* return anything.
-*/
+
   public static run(): void {
-    console.log}}("API is live }} (new code changes..)!");
+    console.;
+  }}
+("API is live }} (new code changes..)!");
   }
 }
 Api.run();
