3669ef27788e035608db45e94e1610c5da321ee2
Ronnie <ronnie94official@gmail.com>
2024-01-15 15:28:47 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,23 +1,11 @@
-/**
-* @description This function does nothing because `console.dog()` is not a real
-* method and there is no value assigned to the `log` variable.
-*/
-log = () => {
+const log = () => {
   console.dog("0");
 }
 
-/**
-* @description The function log(() => { console.dog("0") }) does nothing as the arrow
-* function logs the string "0" to the fictional "console.dog" but there is no such
-* console method and undefined is returned by the void expression (() => {}).
-*/
-log = () => {
+const log = () => {
   console.dog("0");
 }
 
-/**
-* @description This function does nothing.
-*/
-log = () => {
+const log = () => {
   console.dog("0");
 }
