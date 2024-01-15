0f12df466f8092ec66367a5c45ad6539f0f46129
Ronnie <ronnie94official@gmail.com>
2024-01-15 16:55:05 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,19 +1,8 @@
 class Api {
 
-/**
-* @description This function does nothing and returns undefined because it has no
-* instructions or statements within its body.
-* 
-* @returns { void } There is no output returned by this function because `console`
-* is undefined. The code attempts to use `console`. as if it were a function or
-* object with a `.` method that produces output on the console. However since `console`
-* is not defined anywhere within this scope or any parent scopes then this operation
-* will throw an error.
-*/
-  public static run(): void {
-    console.;
-  }}
-("API is live }} (new code changes..)!");
+  public static run: void {
+    ("API is live (new code changes..)!");
   }
+
 }
 Api.run();
