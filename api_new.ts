79fd0208a414c6edb0dad47217d59c7f60d17e62
Ronnie <ronnie94official@gmail.com>
2024-01-15 22:09:26 +0000

    Update api_new.ts

--- api_new.ts
+++ api_new.ts
@@ -1,10 +1,4 @@
 class Api {
-/**
-* @description This function simply logs the message "API is live!" to the console.
-* 
-* @returns { void } The function `run()` has no implementation and only contains a
-* `console.log()` statement.
-*/
   public static run(): void {
     console.log("API is live!");
   }
