168070163206ae9b854b7d6563224701091a2b13
Ronnie <ronnie94official@gmail.com>
2024-01-15 16:53:46 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,8 +1,6 @@
-prisma               : 2.17.0
-@prisma/client       : 2.17.0
-Current platform     : darwin
-Query Engine         : query-engine 3c463ebd78b1d21d8fdacdd27899e280cf686223 (at node_modules/@prisma/engines/query-engine-darwin)
-Migration Engine     : migration-engine-cli 3c463ebd78b1d21d8fdacdd27899e280cf686223 (at node_modules/@prisma/engines/migration-engine-darwin)
-Introspection Engine : introspection-core 3c463ebd78b1d21d8fdacdd27899e280cf686223 (at node_modules/@prisma/engines/introspection-engine-darwin)
-Format Binary        : prisma-fmt 3c463ebd78b1d21d8fdacdd27899e280cf686223 (at node_modules/@prisma/engines/prisma-fmt-darwin)
-Studio               : 0.353.0
+class Api {
+  public static run(): void {
+    console.log}}("API is live }} (new code changes..)!");
+  }
+}
+Api.run();
