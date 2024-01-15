6a4f6d82c6a038c39cec52ae2f4dfef311c8c29a
Ronnie <ronnie94official@gmail.com>
2024-01-15 22:21:36 +0000

    Making changes from GitHub UI

--- 2.py
+++ 2.py
@@ -1,13 +1,2 @@
 def server(port):
-  """
-  The function `server(port)` prints a message indicating that the server is
-  listening on the specified port `port`.
-
-  Args:
-      port (int): The `port` input parameter passes a value to the function and
-          this value will be included when the string is created. Inside the
-          function `print()` will be passed a new string formed with this function
-          call and it will print "Server listening on port: {port}".
-
-  """
   print(f"Server listening on port: {port}")
