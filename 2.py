93216e667d9bb737b1e748925b916d9a9b0347b1
Ronnie <ronnie94official@gmail.com>
2024-01-15 22:29:36 +0000

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,11 +1,2 @@
 def server(port):
-  """
-  This function takes a single argument `port` and prints the message "Server
-  listening on port: <port>".
-
-  Args:
-      port (int): The `port` input parameter specifies the TCP port that the server
-          will listen on and accept incoming connections.
-
-  """
-  print(f"Server listening on port: {port}")
+  print(f"Server listening: {port}")
