bc68a8da771d0776b82aa66047a65873010ae2e5
Ronnie <ronnie94official@gmail.com>
2024-01-15 16:52:53 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,16 +1,8 @@
-// This is your Prisma schema file,
-// learn more about it in the docs: https://pris.ly/d/prisma-schema
-
-datasource db {
-  provider = "postgresql"
-  url      = env("DATABASE_URL")
-}
-
-generator client {
-  provider = "prisma-client-js"
-}
-
-model User {
-  id   Int    @id @default(autoincrement())
-  name String
-}
+prisma               : 2.17.0
+@prisma/client       : 2.17.0
+Current platform     : darwin
+Query Engine         : query-engine 3c463ebd78b1d21d8fdacdd27899e280cf686223 (at node_modules/@prisma/engines/query-engine-darwin)
+Migration Engine     : migration-engine-cli 3c463ebd78b1d21d8fdacdd27899e280cf686223 (at node_modules/@prisma/engines/migration-engine-darwin)
+Introspection Engine : introspection-core 3c463ebd78b1d21d8fdacdd27899e280cf686223 (at node_modules/@prisma/engines/introspection-engine-darwin)
+Format Binary        : prisma-fmt 3c463ebd78b1d21d8fdacdd27899e280cf686223 (at node_modules/@prisma/engines/prisma-fmt-darwin)
+Studio               : 0.353.0
