b90439c7ae1d63bef0dbd6033d0f99d36b5b4013
Ronnie <ronnie94official@gmail.com>
2024-01-15 14:17:43 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-/**
-* @description This function takes a single argument `port` and logs the message
-* "App is running live on port: `<port>`" to the console.
-* 
-* @param { number } port - The `port` input parameter is passed to the function and
-* is used as a argument within the logged message.
-*/
 const app = (port) => {
   console.log("App is running live on port: ", port)
 }
