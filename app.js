e2a1a45b919b3f5452702a68dd53529dd6284b4b
Ronnie <ronnie94official@gmail.com>
2024-01-15 14:15:16 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-/**
-* @description This function creates an empty functional component that logs the
-* message "App is running live!" to the console when the app is loaded.
-*/
 const app = () => {
   console.log("App is running live!");
 };
-
-
