582c14cd897cd397a42bb6e15b22156bcf30a763
Ronnie <ronnie94official@gmail.com>
2024-01-15 16:34:50 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,3 +1,15 @@
-const function f1(number) {
+const TestFunctions = class {
+
+function f1(number) {
   return number * number;
+}
+
+const f2 = function (number) {
+  return number * number;
+};
+
+const f3 = (number) => {
+  return number * number;
+};
+
 }
