73e93bd459aed1efc5295144d94f88b4c8a4903e
Ronnie <ronnie94official@gmail.com>
2024-01-15 16:41:49 +0000

    Update api.js

--- api.js
+++ api.js
@@ -1,11 +1,3 @@
-/**
-* @description This function takes a `number` argument and returns `true`.
-* 
-* @param { number } number - In the given function `f3 = (number) => { return true;
-* }`, the `number` input parameter is not used at all and can be removed altogether.
-* 
-* @returns { boolean } The function `f3` takes a `number` argument and returns `true`.
-*/
-const f3 = (number) => {
+f3 = (number) => {
    return true;
 };
