3f347d7e38b82c96c728121d20b6e5087d89ad1d
Ronnie <ronnie94official@gmail.com>
2024-01-15 17:20:29 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,12 +1,3 @@
 def terminate():
-  """
-  The provided code defines a Python function `terminate` that sets the variable
-  `kill` to 1 and returns it. The function does not actually terminate the program
-  or anything else.
-
-  Returns:
-      int: The output returned by the function `terminate()` is 1.
-
-  """
-  kill = 1;
-  return kill
+  kill = 1
+    return kill
