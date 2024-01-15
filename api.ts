b0f293c661b9e11d8b4e760b8334158a408c5d7f
Ronnie <ronnie94official@gmail.com>
2024-01-15 15:31:35 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,22 +1,7 @@
-/**
-* @description This function does nothing because `console.dog()` is not a valid
-* method and there is no value assigned to the variable `log`.
-*/
-const log = () => {
-  console.dog("0");
-}
-
-/**
-* @description The function `log` does nothing since `console.dog("0")` is undefined.
-*/
-const log = () => {
-  console.dog("0");
-}
-
-/**
-* @description This function does nothing because `console.dog` is a nonexistent
-* method and will not print anything to the console.
-*/
-const log = () => {
-  console.dog("0");
-}
+const addTwo = (num) => {return num + 2;};
+const addTwo = (num) => num + 2;
+const addTwo = num => num + 2;
+const addTwo = a => {
+ const newValue = a + 2;
+ return newValue;
+};
