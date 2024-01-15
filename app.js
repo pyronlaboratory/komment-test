cb6c5d272c057883b183a7f165829114bd099ba8
Ronnie <ronnie94official@gmail.com>
2024-01-15 14:17:03 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-/**
-* @description The function app creates an anonymous function that outputs the message
-* "App is running live!" to the console once.
-*/
 const app = () => {
   console.log("App is running live!");
 };
-
-
