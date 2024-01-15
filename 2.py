6524de758d8f2048a217fa45ded7ac1d70f74fe6
pyronlaboratory <ronnie94official@gmail.com>
2024-01-15 22:28:31 +0000

    Local commit

--- 2.py
+++ 2.py
@@ -1,11 +1,2 @@
 def server(port):
-  """
-  The function "server" takes a single argument "port", and prints a message
-  indicating that the server is listening on that port.
-
-  Args:
-      port (int): The `port` input parameter is a keyword argument that specifies
-          which port the function should listen on.
-
-  """
   print(f"Server listening on port: {port}")
