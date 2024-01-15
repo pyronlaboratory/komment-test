2b15ec308eea1ee03d9ce52492decf08ff422a70
Ronnie <ronnie94official@gmail.com>
2024-01-15 14:30:16 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-/**
-* @description The function creates an empty function expression and then logs the
-* message "App is running live!" to the console using `console.log()`.
-*/
 const app = () => {
   console.log("App is running live!");
 };
