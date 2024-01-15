27e4e61fc3176c7f066bf3d8aea1494ea5ad87d1
Ronnie <ronnie94official@gmail.com>
2024-01-15 15:24:41 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,9 +1,2 @@
-/**
-* @description The function `log()` does nothing because it is undefined. The arrow
-* function declared with no name or parameters has an empty body `()` and immediately
-* returns undefined.
-*/
 const log = () =>  {
   console.dog("0");
-}
-
