deb963d6048f091d1fabebf0bfd4b87e87c70384
Ronnie <ronnie94official@gmail.com>
2024-01-15 17:19:44 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,10 +1,3 @@
 def terminate():
-  """
-  The given function "terminate" sets the variable "kill" to 1 and returns it.
-
-  Returns:
-      int: The output returned by the `terminate` function is 1.
-
-  """
-  kill = 1
+  kill = 1;
   return kill
