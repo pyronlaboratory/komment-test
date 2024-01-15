e00e905a8c795b8fd52e15c645592d139fc6b368
pyronlaboratory <ronnie94official@gmail.com>
2024-01-15 22:36:11 +0000

    Local commit with different set of files

--- 2.js
+++ 2.js
@@ -1,11 +1,3 @@
-
-/**
-* @description This function takes a single argument `port` and logs a message to
-* the console indicating that services are being queued on that port.
-* 
-* @param { number } port - The `port` input parameter is passed to the function and
-* simply logged to the console.
-*/
 const services = (port) => {
     console.log("Services are being queued on port: ", port)
 }
--- 2.py
+++ 2.py
@@ -1,12 +1,2 @@
 def server(port):
-  """
-  The `server` function listens for incoming requests on a specific port (as
-  specified by the input `port`) and prints a message indicating that it is ready
-  to serve requests.
-
-  Args:
-      port (int): The `port` input parameter passed to the `server()` function
-          specifies the TCP port on which the server will listen for incoming connections.
-
-  """
   print(f"Server listening: {port}")
