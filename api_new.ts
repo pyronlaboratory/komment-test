337d16b0dc505c6e266bad7ecc2664248e9bf088
Ronnie <ronnie94official@gmail.com>
2024-01-15 17:29:07 +0000

    Update api_new.ts

--- api_new.ts
+++ api_new.ts
@@ -1,13 +1,6 @@
 class Api {
-/**
-* @description This function logs the message "API is live (new code changes..)!".
-* 
-* @returns { void } The output of this function is:
-* 
-* "API is live (new code changes..)!"
-*/
   public static run(): void {
-    console.log("API is live (new code changes..)!");
+    console.log("API is live!");
   }
 }
 Api.run();
