aa7613dd0ccbac9eca8db1552946d6444ee8ffa9
pyronlaboratory <ronnie94official@gmail.com>
2024-01-16 14:05:36 +0000

    Local commit

--- 1 (1).js
+++ 1 (1).js
@@ -1,11 +1,3 @@
-/**
-* @description This function takes a single argument `port` and logs the message
-* "App is running live on port: " followed by the value of `port`.
-* 
-* @param { number } port - The `port` input parameter passes a value to the function
-* that gets logged by the `console.log()` method alongside the message "App is running
-* live on port".
-*/
 const app = (port) => {
   console.log("App is running live on port: ", port)
 }
