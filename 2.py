a76f7907b61f5c30205f215aeff982eece0518dc
Ronnie <ronnie94official@gmail.com>
2024-01-16 15:06:08 +0000

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,12 +1,3 @@
 def server(port):
-  """
-  The given function "server(port)" prints a message indicating that the server
-  is listening on the specified port.
-
-  Args:
-      port (int): The `port` input parameter is used to specify the port number
-          that the server should listen on.
-
-  """
   print(f"Server listening: {port}")
 
