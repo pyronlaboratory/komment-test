717412e34b6eaddd8f20e205c78a923a4900c0c3
pyronlaboratory <ronnie94official@gmail.com>
2024-01-16 18:36:20 +0000

    New local commit

--- 1 (2).js
+++ 1 (2).js
@@ -1,18 +1,3 @@
-/**
-* @description This function takes a single argument `port`, logs a message to the
-* console indicating that authentication is being redirected to port `port`, and
-* then... nothing.
-* 
-* @param { number } port - The `port` input parameter passed to the `auth` function
-* simply logs a message to the console indicating the specific port being used for
-* authentication.
-*/
 const auth = (port) => {
   console.log("Authentication is redirecting on port: ", port)
-
-
-
-
-
-  
 }
--- 2.js
+++ 2.js
@@ -1,10 +1,3 @@
-/**
-* @description The given function `services` takes a single argument `port`, and
-* logs a message to the console indicating that services are being queued on that port.
-* 
-* @param { number } port - The `port` input parameter is passed to the `console.log()`
-* function and logs the string "Services are being queued on port: ".
-*/
 const services = (port) => {
     console.log("Services are being queued on port: ", port)
 }
