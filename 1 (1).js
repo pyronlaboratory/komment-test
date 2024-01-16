d29dcf72427bbb66f782e2097c7e58d6267367be
pyronlaboratory <ronnie94official@gmail.com>
2024-01-16 18:27:12 +0000

    Direct bulk commit

--- 1 (1).js
+++ 1 (1).js
@@ -1,10 +1,3 @@
-/**
-* @description This function takes a single argument `port`, and logs the message
-* "App is running live on port: " followed by the value of `port` to the console.
-* 
-* @param { number } port - The `port` input parameter specifies the TCP port where
-* the app should listen for incoming connections.
-*/
 const app = (port) => {
   console.log("App is running live on port: ", port)
 }
--- 2.py
+++ 2.py
@@ -1,13 +1,4 @@
 def server(port):
-  """
-  The function "server" listens for incoming requests on a specific port (parameter)
-  and prints a message indicating that it is ready to receive connections.
-
-  Args:
-      port (int): The `port` input parameter tells the function which port number
-          to listen on for incoming requests.
-
-  """
   print(f"Server listening: {port}")
 
 
