8ad599d0b8f69a610cf2bf0959d7aa00d5099829
pyronlaboratory <ronnie94official@gmail.com>
2024-01-16 18:48:45 +0000

    More local changes

--- 🤖.ts
+++ 🤖.ts
@@ -1,10 +1,4 @@
 class Api {
-/**
-* @description The function `run()` simply logs the message "API is live!" to the console.
-* 
-* @returns { void } The output of the function is "API is live!" because the
-* `console.log()` statement within the function prints that string to the console.
-*/
   public static run(): void {
     console.log("API is live!");
   }
