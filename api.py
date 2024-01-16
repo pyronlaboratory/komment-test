02dd504dfd5362c10f4cd88c639fc643e8d18234
Ronnie <ronnie94official@gmail.com>
2024-01-16 09:21:26 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,3 @@
 def terminate():
-  """
-  This function prints the string "ps -kill".
-
-  Returns:
-      str: The output returned by the `terminate` function is "ps -kill".
-
-  """
+pass
   return "ps -kill"
