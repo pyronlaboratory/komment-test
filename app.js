6cc6a1bd4259a417215a72d9dcdac1f545707a45
Ronnie <ronnie94official@gmail.com>
2024-01-19 15:55:26 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-/**
-* @description This function doesn't do anything since `undefined` is not a function
-* and nothing gets executed when it's called.
-*/
+
 const app = () => {
   console.log("App is running live!");
 };
