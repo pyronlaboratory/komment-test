0e70e5d5cfb85f59bef41bc4ea2e4bbca6c68539
Ronnie <ronnie94official@gmail.com>
2024-01-26 11:20:25 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,3 @@
 def terminate():
-  """
-  This function does nothing. The body of the function is simply "True", which is
-  a truthy value that always evaluates to True. In other words: this function does
-  not perform any action and always returns True.
-
-  """
   True
 
