40421c9e49bbee81df3ab54276d8bce19d77f9d6
Ronnie <ronnie94official@gmail.com>
2024-01-29 18:08:02 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,4 @@
 def terminate():
-  """
-  This function does nothing. The `True` value returned from the `terminate()`
-  function is an empty truthy value that has no effect.
-
-  """
-  True
+True
 
 
