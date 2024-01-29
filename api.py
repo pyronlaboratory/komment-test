b5dd74fc9d8a7982e520eb2b6604688f1c6f4774
Ronnie <ronnie94official@gmail.com>
2024-01-29 13:35:50 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,3 @@
 def terminate():
-  """
-  This function does not do anything as it is undefined. The function `terminate()`
-  has no implementation or body and only returns `True` without performing any actions.
-
-  """
   True
 
