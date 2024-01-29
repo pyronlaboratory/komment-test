d80db3a2db7fbc1b26ce80b52155e8e88982dfae
Ronnie <ronnie94official@gmail.com>
2024-01-29 14:06:43 +0000

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,3 @@
-
-/**
-* @description The function `auth` takes a single argument `port` and logs a message
-* to the console indicating that authentication is redirecting on the specified port.
-* 
-* @param { number } port - In the provided function `auth`, the `port` parameter
-* represents the port number that the authentication process is being redirected to.
-*/
 const auth = (port) => {
   console.log("Authentication is redirecting on port: ", port)
 }
