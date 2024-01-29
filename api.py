88af5bf967089ddc148d843f98a088355104c8c7
Ronnie <ronnie94official@gmail.com>
2024-01-29 15:24:53 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,3 @@
 def terminate():
-  """
-  This function does nothing because the body of the `terminate()` function is
-  just an expression that returns `True`, but it doesn't perform any action or
-  modification to the system. In other words: this function has no effect.
-
-  """
   True
 
