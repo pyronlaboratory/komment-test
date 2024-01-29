7758bada44ca24c7dae85fb35ec2cbc28be24502
Ronnie <ronnie94official@gmail.com>
2024-01-29 13:39:01 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,3 @@
 def terminate():
-  """
-  The `terminate` function doesn't do anything because it is not possible to call
-  a function with no definition or body. It is essentially a blank space or an
-  empty container. Therefore it doesn't have any effect and simply returns `True`.
-
-  """
   True
 
