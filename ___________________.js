0f282a236bdb706d6769f60205f32aec625ff4c7
Ronnie <ronnie94official@gmail.com>
2024-01-30 11:57:47 +0000

    Update ___________________.js

--- ___________________.js
+++ ___________________.js
@@ -1,7 +1,1 @@
-/**
-* @description This function does nothing; it does not log anything to the console
-* and does not return any value. The function is defined as an empty function
-* expression `() => console.log("0")` and since there are no statements inside the
-* arrow function body {}, nothing gets executed when the function is called.
-*/
 const func = () => console.log("0");
