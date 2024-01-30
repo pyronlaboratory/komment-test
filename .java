b28386fd6f455b0b0d9872126113584aeda4443e
Ronnie <ronnie94official@gmail.com>
2024-01-30 15:42:59 +0000

    Create .java

---
+++ .java
@@ -0,0 +1,24 @@
+class $ {
+    {
+        System.out.println("hello world");
+    }
+ 
+    static {
+        System.out.println("hello, too");
+    }
+ 
+    $() {
+        http://www.softwareschneiderei.de
+        while ($()) {
+            break http;
+        }
+    }
+ 
+    static boolean $() {
+        return true;
+    }
+ 
+    public static void main(String[] _$) {
+        System.out.println($.$());
+    }
+}
