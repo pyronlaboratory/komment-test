3278fe2d0dbb2d6cb555a86be68870f3f4967d9f
Ronnie <ronnie94official@gmail.com>
2024-01-30 11:43:26 +0000

    Update ___________________.js

--- ___________________.js
+++ ___________________.js
@@ -1,5 +1,2 @@
-/**
-* @description This function does nothing. The `()` syntax is an empty function
-* definition and the `console.log("0")` statement is not executed.
-*/
+
 const func = () => console.log("0");
