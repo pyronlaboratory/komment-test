2f89a01b98f2448aab48b2117da786a4dfa1c3c5
Ronnie <ronnie94official@gmail.com>
2024-01-30 12:01:56 +0000

    Update ___________________.js

--- ___________________.js
+++ ___________________.js
@@ -1,5 +1,1 @@
-/**
-* @description The function takes no arguments and returns nothing. When called (for
-* example: `func()`), it logs the string "0" to the console.
-*/
 const func = () => console.log("0");
