df63cdeda073e795722cc10a0680d967d05f8bfc
Ronnie <ronnie94official@gmail.com>
2024-01-30 13:13:05 +0000

    Update ___________________.js

--- ___________________.js
+++ ___________________.js
@@ -1,6 +1,2 @@
 
-/**
-* @description This function does nothing. The `()` empty function expression returns
-* undefined and the `console.log("0")` statement is not executed.
-*/
 const func = () => console.log("0");
