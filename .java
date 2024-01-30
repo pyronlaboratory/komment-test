b9b6d20950559154dafbd58da959183d4ef2b0c5
Ronnie <ronnie94official@gmail.com>
2024-01-30 15:45:23 +0000

    Update .java

--- .java
+++ .java
@@ -2,24 +2,19 @@ class $ {
     {
         System.out.println("hello world");
     }
- 
     static {
         System.out.println("hello, too");
     }
- 
     $() {
         http://www.softwareschneiderei.de
         while ($()) {
             break http;
         }
     }
- 
     static boolean $() {
         return true;
     }
- 
     public static void main(String[] _$) {
         System.out.println($.$());
     }
-    
 }
