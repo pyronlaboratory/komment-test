0f0ce67f3709afd62c49370a0119ac0055206c92
Ronnie <ronnie94official@gmail.com>
2024-01-30 11:57:18 +0000

    Update ___________________.js

--- ___________________.js
+++ ___________________.js
@@ -1,5 +1,1 @@
-/**
-* @description The function takes no arguments and only has a single statement
-* `console.log("0")` which will print the string "0" to the console.
-*/
 const func = () => console.log("0");
