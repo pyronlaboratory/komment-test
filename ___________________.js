785c75f1c0488fc2d4f0431ff222bfd09604648d
Ronnie <ronnie94official@gmail.com>
2024-01-30 12:05:39 +0000

    Update ___________________.js

--- ___________________.js
+++ ___________________.js
@@ -1,5 +1,2 @@
-/**
-* @description The function `func` is defined as an arrow function that simply logs
-* the string "0" to the console using `console.log`.
-*/
+
 const func = () => console.log("0");
