af7c69c9b3eb927680e279b0084482eb2eba2c82
Ronnie <ronnie94official@gmail.com>
2024-01-30 11:46:01 +0000

    Update ___________________.js

--- ___________________.js
+++ ___________________.js
@@ -1,5 +1,1 @@
-/**
-* @description This function doesn't do anything. It logs "0" to the console once
-* but does not return any value and has no side effects.
-*/
 const func = () => console.log("0");
