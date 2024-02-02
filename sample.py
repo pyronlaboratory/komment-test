11af2de163d9e4b1e38cbd7d2d70a79c5783a230
Ronnie <ronnie94official@gmail.com>
2024-02-02 13:27:39 +0000

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,26 @@
+def create_deployment_object():
+    container = client.V1Container(
+        name="nginx-sample",
+        image="nginx",
+        image_pull_policy="IfNotPresent",
+        ports=[client.V1ContainerPort(container_port=80)],
+    )
+    # Template
+    template = client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(labels={"app": "nginx"}),
+        spec=client.V1PodSpec(containers=[container]))
+    # Spec
+    spec = client.V1DeploymentSpec(
+        replicas=1,
+        selector=client.V1LabelSelector(
+            match_labels={"app": "nginx"}
+        ),
+        template=template)
+    # Deployment
+    deployment = client.V1Deployment(
+        api_version="apps/v1",
+        kind="Deployment",
+        metadata=client.V1ObjectMeta(name="deploy-nginx"),
+        spec=spec)
+
+    return deployment
