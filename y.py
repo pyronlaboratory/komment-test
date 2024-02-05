4ff5ad6e2f14ae5ab9ba0bc2b44c6eee26b0a880
Ronnie <ronnie94official@gmail.com>
2024-02-05 13:44:38 +0000

    Update y.py

--- y.py
+++ y.py
@@ -1,3 +1,19 @@
+def fit(self, x_train, y_train, epochs, learning_rate):
+  samples = len(x_train)
+  for i in range(epochs):
+    err = 0
+    for j in range(samples):
+      output = x_train[j]
+      for layer in self.layers:
+	output = layer.forward_propagation(output)
+	err += self.loss(y_train[j], output)
+	error = self.loss_prime(y_train[j], output)
+	for layer in reversed(self.layers):
+	  error = layer.backward_propagation(error, learning_rate)
+	err /= samples
+	print("epoch %d/%d   error=%f" % (i + 1, epochs, err))
+
+
 class Network:
 	def __init__(self):
 		"""
