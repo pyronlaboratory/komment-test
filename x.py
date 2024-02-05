bdfbfe253f1ef8630b2d923df91a4a52a6cb08b0
Ronnie <ronnie94official@gmail.com>
2024-02-05 14:48:48 +0000

    Create x.py

---
+++ x.py
@@ -0,0 +1,19 @@
+    @pytest.mark.task(taskno=2)
+    def test_get_change(self):
+        test_data = [(463000, 5000), (1250, 120), (15000, 1380)]
+        result_data = [458000, 1130, 13620]
+
+        for variant, (params, expected) in enumerate(zip(test_data, result_data), start=1):
+            budget, exchanging_value = params
+
+            with self.subTest(f"variation #{variant}",
+                              budget=budget,
+                              exchanging_value=exchanging_value,
+                              expected=expected):
+
+                actual_result = get_change(*params)
+                error_message = (f'Called get_change{budget, exchanging_value}. '
+                                 f'The function returned {actual_result}, but '
+                                 f'The tests expected {expected} left in your budget.')
+
+                self.assertAlmostEqual(actual_result, expected, msg=error_message)
