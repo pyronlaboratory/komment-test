d6dd18ac9784cee8510e6500764e484929c560ad
Ronnie <ronnie94official@gmail.com>
2024-02-05 13:49:18 +0000

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,3 +1,15 @@
+const search = (arr, x, start, end) => {
+  if (start > end) return false;
+  let mid = Math.floor((start + end) / 2);
+  if (arr[mid] === x) return true;
+  if (arr[mid] > x) {
+    return search(arr, x, start, mid - 1);
+  } else {
+    return search(arr, x, mid + 1, end);
+  }
+};
+
+
 /**
  * @description retrieves the value of an AppID  based on an application name within
  * a CSV file
