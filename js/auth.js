c210d39a1b0f705c7c45578bfaf78f6908d8ebbd
Ronnie <ronnie94official@gmail.com>
2024-02-06 17:43:08 +0000

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,10 +1,10 @@
-/**
-* @description This function takes a single argument `port` and logs a message to
-* the console indicating that authentication is being redirected to the specified port.
-* 
-* @param { number } port - The `port` input parameter is a reference to the port
-* number where the authentication will be redirected to.
-*/
-const auth = (port) => {
-  console.log("Authentication is redirecting on port: ", port)
-}
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss')('./src/css/tailwind.config.js'),
+        require('autoprefixer')
+      ],
+    },
+  },
+};
