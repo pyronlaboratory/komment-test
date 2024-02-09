9a3575ae1cfd7e25f7aff1abb07a154899ab3f9f
Ronnie <ronnie94official@gmail.com>
2024-02-09 09:05:35 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 
-/**
- * @description The anonymous self-invoking function(() => {}) logs the message "App
- * is running live!" to the console when executed.
- */
 const app = () => {
   console.log("App is running live!");
 };
