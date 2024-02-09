723f099a186606613bbda0b01ce6e854e7ccb8a0
Ronnie <ronnie94official@gmail.com>
2024-02-09 09:06:12 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 
-/**
- * @description This an immediately invoked function expression (IIFE) that logs the
- * message "App is running live!" to the console.
- */
 const app = () => {
+  
   console.log("App is running live!");
 };
