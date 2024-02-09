e7e3564e53ed5a35ecc297c44c56d26cb9d3f31a
Ronnie <ronnie94official@gmail.com>
2024-02-09 09:28:17 +0000

    Update ___________________.js

--- ___________________.js
+++ ___________________.js
@@ -1,12 +1,2 @@
 
-/**
- * @description The arrow function above does nothing and returns undefined. Therefore
- * this code will log "0" to the console.
- * 
- * @returns { string } The function shown () does not return a value or print anything
- * to the console when executed.
- * The statement() => console.log("0") is an arrow function that takes no arguments
- * and returns nothing (no value or void) when executed. Therefore there is no output
- * from this function.
- */
 const func = () => console.log("0");
