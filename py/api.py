5f44f511657f1cfc59396652982a40da33cbe5d5
Ronnie <ronnie94official@gmail.com>
2024-02-12 20:26:11 +0000

    Update api.py

--- py/api.py
+++ py/api.py
@@ -1,11 +1,2 @@
 def terminate():
-  
-  """
-  This function returns "True" and does not perform any other action; it simply
-  returns a true value when called.
-
-  Returns:
-      bool: The output returned by the `terminate()` function is `True`.
-
-  """
   return True
