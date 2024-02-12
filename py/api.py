95715492bed66e38242b5eb387e6acc7444940bf
Ronnie <ronnie94official@gmail.com>
2024-02-12 20:17:55 +0000

    Update api.py

--- py/api.py
+++ py/api.py
@@ -1,9 +1,2 @@
 def terminate():
-  """
-  This function returns "True" and thus prompts the program to stop executing.
-
-  Returns:
-      bool: The function returns "True".
-
-  """
   return True
