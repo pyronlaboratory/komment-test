2f44c14a92f335cc1e53ad6f995daee389e7e9f8
Ronnie <ronnie94official@gmail.com>
2024-02-14 11:40:00 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -33,19 +33,7 @@ const ReactCurrentOwner = ReactSharedInternals.ReactCurrentOwner;
 const ReactDebugCurrentFrame = ReactSharedInternals.ReactDebugCurrentFrame;
 
 
-/**
- * @description This function sets the current validation element based on development
- * environment and conditionally records the type of element with its corresponding
- * source location into an extra stack frame for debugging purposes.
- * 
- * @param {  } element - In this specific function context， the `element` input does
- * two things:
- * 
- * 1) If no element is provided as an input to setCurrentlyValidatingElement function
- * then setExtraStackFrame null
- * 
- * 2)If an element is provided setExtraStackFrame with information regarding it
- */
+
 function setCurrentlyValidatingElement(element) {
   
   if (__DEV__) {
