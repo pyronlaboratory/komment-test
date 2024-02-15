32a730db2e3d56e3f082ac6cac3a35813279ef30
Ronnie <ronnie94official@gmail.com>
2024-02-15 14:32:19 +0000

    Update 2.py

--- py/2.py
+++ py/2.py
@@ -1,12 +1,2 @@
 def server(port):
-  """
-  This function is a server that listens on a specific port and prints a message
-  indicating that it is running and ready to accept connections.
-
-  Args:
-      port (int): The port parameter represents the TCP port number on which the
-          server listens for incoming connections. The server will listen only if
-          there is an incoming connection to that port.
-
-  """
   print(f"Server listening: {port} | adding new changes in parallel while output channel mode changed")
