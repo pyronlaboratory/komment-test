1507928e8d42068b969b011940c3375a61129ed7
Ronnie <ronnie94official@gmail.com>
2024-02-15 14:31:43 +0000

    Update 2.py

--- py/2.py
+++ py/2.py
@@ -1,12 +1,2 @@
 def server(port):
-  """
-  This Python function defines a simple web server that listens on a specific port
-  and prints a message to the console indicating that it is running and ready to
-  receive requests.
-
-  Args:
-      port (int): The `port` parameter determines which port number the server
-          will listen on for incoming requests.
-
-  """
   print(f"Server listening: {port} | adding new changes in parallel while output channel mode changed")
