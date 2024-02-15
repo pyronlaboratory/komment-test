062b8b6118aa033f609dd559a8dbc08b4fef50f9
Ronnie <ronnie94official@gmail.com>
2024-02-15 21:37:40 +0000

    Update 2.py

--- py/2.py
+++ py/2.py
@@ -1,14 +1,2 @@
 def server(port):
-  
-  """
-  This Python function "server" accepts a port number as an argument and prints a
-  message indicating that the server is now running and listening for incoming
-  requests on the specified port. Additionally it mentions about implementing
-  parallel changes while changing the output channel mode.
-
-  Args:
-      port (int): The input parameter 'port' specifies the port number that the
-          server listens to for incoming connections.
-
-  """
   print(f"Server listening: {port} | adding new changes in parallel while output channel mode changed")
