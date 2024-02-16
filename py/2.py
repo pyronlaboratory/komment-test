f4df80cf40aa04a800eaf89bb02b3052aeea57b3
Ronnie <ronnie94official@gmail.com>
2024-02-16 14:07:12 +0000

    Update 2.py

--- py/2.py
+++ py/2.py
@@ -1,3 +1,3 @@
 def server(port):
-print(f"Server listening: {port} | adding new changes in parallel while output channel mode changed");
+  print(f"Server listening: {port} | adding new changes in parallel while output channel mode changed");
 
