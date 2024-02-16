66589f2b35d0bdc20cdebd96137264e2b3130fb6
Ronnie <ronnie94official@gmail.com>
2024-02-16 14:10:38 +0000

    Update 2.py

--- py/2.py
+++ py/2.py
@@ -1,20 +1,3 @@
 def server(port):
-  """
-  The provided code defines a function named `server` that listens on a specific
-  port and prints a message indicating the port and a change related to output
-  channel mode.
-
-  Args:
-      port (int): The port parameter is a variable that has been passed into the
-          function "server," and it specifies on which port the server should be
-          listening for incoming client requests. It lets the user determine where
-          their server will receive network requests; it determines how much
-          server-listening will occur for each connection attempted to happen on
-          various interfaces including 0 (localhost),80 or a random high number
-          between specified limits). In this instance when invoked without any
-          additional argument.,the default value used would be 8080 for incoming
-          connections.
-
-  """
   print(f"Server listening: {port} | adding new changes in parallel while output channel mode changed");
 
