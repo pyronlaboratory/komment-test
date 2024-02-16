399e0add7e501afa09334a38a0cd0dc3757659ac
Ronnie <ronnie94official@gmail.com>
2024-02-16 14:15:44 +0000

    Update 2.py

--- py/2.py
+++ py/2.py
@@ -1,13 +1,3 @@
 def server(port):
-  """
-  This Python function takes a port number as input and prints a message indicating
-  that the server is listening on that port and that new changes will be added to
-  the system while the output channel's mode is changed.
-
-  Args:
-      port (dict): The `port` input parameter defines the TCP port that the server
-          listens on and accepts incoming connections from clients.
-
-  """
   print(f"Server listening: {port} | adding new changes in parallel while output channel mode changed");
 
