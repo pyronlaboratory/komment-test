4deffa4a1843b7358120d3bbaca3dd7940833555
Ronnie <ronnie94official@gmail.com>
2024-02-19 12:31:04 +0000

    Update 0.py

--- py/0.py
+++ py/0.py
@@ -4,7 +4,6 @@ from typing import List, Tuple
 def terminate():
     pass
 
-
 def default_matrix_multiplication(a: List, b: List) -> List:
     if len(a) != 2 or len(a[0]) != 2 or len(b) != 2 or len(b[0]) != 2:
         raise Exception("Matrices are not 2x2")
