355b89f46b92c09eea304b01003842510a4954dd
Ronnie <ronnie94official@gmail.com>
2024-02-19 16:01:37 +0000

    Update ___________________.js

--- js/___________________.js
+++ js/___________________.js
@@ -2,22 +2,11 @@
 
 function foo() {
 }
-/**
- * @description Nothing; it returns void.
- * 
- * @returns {  } There is no output returned by this function. The `return` statement
- * within the function indicates that control will be transferred back to the caller
- * of the function and no value will be returned.
- */
+
 function foo() {
     return;
 }
 
-/**
- * @description The function named foo returns undefined.
- * 
- * @returns {  } Output of the function is undefined.
- */
 function foo() {
     return undefined;
 }
