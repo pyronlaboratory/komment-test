4214b1924b046dc5e25139aac415b2ab8662129f
Ronnie <ronnie94official@gmail.com>
2024-03-04 23:42:14 +0000

    Update sample.cs

--- cs/sample.cs
+++ cs/sample.cs
@@ -5,3 +5,10 @@ abstract class Motorcycle
     public virtual int Drive(int miles, int speed) { /* Method statements here */ return 1; }
     public abstract double GetTopSpeed();
 }
+
+public Point Move(int dx, int dy) => new Point(x + dx, y + dy);
+public void Print() => Console.WriteLine(First + " " + Last);
+// Works with operators, properties, and indexers too.
+public static Complex operator +(Complex a, Complex b) => a.Add(b);
+public string Name => First + " " + Last;
+public Customer this[long id] => store.LookupCustomer(id);
