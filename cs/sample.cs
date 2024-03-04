be7fe556a255ca301d2f5bb7c80adb5865229ad4
Ronnie <ronnie94official@gmail.com>
2024-03-04 23:47:28 +0000

    Update sample.cs

--- cs/sample.cs
+++ cs/sample.cs
@@ -6,9 +6,16 @@ abstract class Motorcycle
     public abstract double GetTopSpeed();
 }
 
+
 public Point Move(int dx, int dy) => new Point(x + dx, y + dy);
 public void Print() => Console.WriteLine(First + " " + Last);
 // Works with operators, properties, and indexers too.
 public static Complex operator +(Complex a, Complex b) => a.Add(b);
 public string Name => First + " " + Last;
 public Customer this[long id] => store.LookupCustomer(id);
+
+
+using System;
+namespace GoogleTestAdapter.Common { public static class Functional {public static Func<TArgument, TAnonymousReturnType> ToFunc<TArgument, TAnonymousReturnType>(Func<TArgument, TAnonymousReturnType> lambda)
+{return lambda;}}}
+
