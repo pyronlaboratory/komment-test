a083a13a92b550b0cb3cc1e568c68e566e270087
Ronnie <ronnie94official@gmail.com>
2024-03-04 23:50:19 +0000

    Create methods.cs

---
+++ cs/methods.cs
@@ -0,0 +1,14 @@
+public static class StaticClass  
+{  
+    public static int DoSomething()  
+    {  
+        return 2;  
+    }  
+}  
+public class Program  
+{  
+    static void Main()  
+    {  
+        StaticClass.DoSomething();  
+    }  
+}  
