793ce432d93862187afc22c9d2495a4af360512a
Ronnie <ronnie94official@gmail.com>
2024-03-06 09:55:49 +0000

    Update anon.py.cs

--- cs/anon.py.cs
+++ cs/anon.py.cs
@@ -1,1 +1,13 @@
 System.Linq.Expressions.Expression<Func<int, int>> e = x => x * x;
+
+using System;
+using System.Linq.Expressions;
+
+class Program
+{
+    static void Main()
+    {
+        Expression<Func<int, int>> e = x => x * x;
+    }
+}
+
