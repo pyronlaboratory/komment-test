b7800c97df8b1394bf42278e1739c04cc87ae177
Ronnie <ronnie94official@gmail.com>
2024-03-06 09:18:20 +0000

    Update fn.cs

--- cs/fn.cs
+++ cs/fn.cs
@@ -1,3 +1,6 @@
-using System;
-namespace GoogleTestAdapter.Common { public static class Functional {public static Func<TArgument, TAnonymousReturnType> ToFunc<TArgument, TAnonymousReturnType>(Func<TArgument, TAnonymousReturnType> lambda)
-{return lambda;}}}
+// using System;
+// namespace GoogleTestAdapter.Common { public static class Functional {public static Func<TArgument, TAnonymousReturnType> ToFunc<TArgument, TAnonymousReturnType>(Func<TArgument, TAnonymousReturnType> lambda)
+// {return lambda;}}}
+
+using System;namespace GoogleTestAdapter.Common{public static class Functional{public static Func<TArgument, TAnonymousReturnType> ToFunc<TArgument, TAnonymousReturnType>(Func<TArgument, TAnonymousReturnType> lambda){return lambda;}}class Program{static void Main(string[] args){Func<int, int> square = x => x * x;Func<int, int> cube = x => x * x * x;Func<int, int> squareFunc = Functional.ToFunc(square);Func<int, int> cubeFunc = Functional.ToFunc(cube);Console.WriteLine("Square of 5: " + squareFunc(5));Console.WriteLine("Cube of 3: " + cubeFunc(3));}}}
+
