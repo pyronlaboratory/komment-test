8ebd018e1faf37616d1221e617b7fc9447e4cf83
Ronnie <ronnie94official@gmail.com>
2024-03-06 10:24:15 +0000

    Update 🐛.cs

--- cs/🐛.cs
+++ cs/🐛.cs
@@ -6,3 +6,39 @@ void empty()
 void Foo() => Expression.Empty();
 
 Func<int, int, int> f = (x, y) => x + y;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 10;
+        int num2 = 0;
+        
+        int result = Divide(num1, num2);
+        
+        Console.WriteLine("The result is: " + result);
+    }
+    
+    static int Divide(int dividend, int divisor)
+    {
+        if (divisor == 0)
+        {
+            Console.WriteLine("Error: Cannot divide by zero");
+            return 0;
+        }
+        else
+        {
+            return dividend / divisor;
+        }
+    }
+    
+    static void SomeOtherFunction()
+    {
+        Console.WriteLine("Inside SomeOtherFunction");
+        // Uncommenting this line will cause an error
+        int x = "Hello";
+    }
+}
+
