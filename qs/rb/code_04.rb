f294f7b5c809c010d0e7538361ebe6d878da4505
pyronlaboratory <ronnie94official@gmail.com>
2024-03-07 11:27:51 +0000

    Batch update with file deletion

--- qs/rb/code_04.rb
+++ qs/rb/code_04.rb
@@ -1,15 +1,4 @@
 class Test
-  # sets the `@active` instance variable to the provided `active` argument.
-  # 
-  # @param active [`Boolean`.] state of the object, setting it to `true` if it is
-  # active and `false` otherwise when the function is called.
-  # 
-  # 		- `@active`: A boolean attribute that represents the activity state of the object.
-  # It takes on the value of `active` passed to the function as its argument.
-  # 
-  # 
-  # @returns [Module] an instance variable `@active` that stores the input parameter
-  # `active`.
   def initialize(active)
     @active = active
   end
--- qs/rb/♦️.rb
+++ qs/rb/♦️.rb
@@ -1,27 +1,11 @@
 class X
-   # prints "x:" and then calls two inner functions, `nested_y` and `nested_z`, which
-   # print "ha!" and then call `nested_y` again.
-   # 
-   # @returns [`Expression`.] :
-   # 
-   # "x: ha! z:"
-   # 
-   # 		- `x:` - A single line of text printed to the console.
-   # 		- `ha!`: - A single line of text printed to the console within the `nested_y` function.
-   # 		- `z:` - A single line of text printed to the console within the `nested_z` function.
-   # 		- `nested_y` and ` nested_z` - Two functions defined within the `outer_x` function,
-   # which print additional lines of text to the console.
    def outer_x
       print( "x:" )
-      # prints "ha!" to the console.
-      # 
-      # @returns [String] "ha! ".
+
       def nested_y
          print("ha! ")
       end
-      # prints the value of `z`.
-      # 
-      # @returns [Integer] "z: ...".
+
       def nested_z
          print( "z:" )
          nested_y
