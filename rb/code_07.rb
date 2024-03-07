385da1806a8606e995e67447f9489bb50e701e9a
Ronnie <ronnie94official@gmail.com>
2024-03-07 10:46:30 +0000

    Update code_07.rb

--- rb/code_07.rb
+++ rb/code_07.rb
@@ -1,12 +1,3 @@
-class Overruler
-    def [] (input)
-          if input.instance_of?(String)
-            puts "string"
-          else
-            puts "not string"
-          end
-     end
-end
-foo = Overruler.new
-foo["bar"].inspect
-foo[1].inspect
+  def initialize(text)
+    @text = text
+  end
