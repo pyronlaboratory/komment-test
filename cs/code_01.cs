f5e580a9d50b41c7b6f33b4e1b12a7830b61114b
Ronnie <ronnie94official@gmail.com>
2024-03-07 12:21:10 +0000

    Update code_01.cs

--- cs/code_01.cs
+++ cs/code_01.cs
@@ -1,3 +1,16 @@
+public static IEnumerable<T> Sample<T>(this IEnumerable<T> sequence, int frequency, 
+    [CallerArgumentExpression(nameof(sequence))] string? message = null)
+{
+    if (sequence.Count() < frequency)
+        throw new ArgumentException($"Expression doesn't have enough elements: {message}", nameof(sequence));
+    int i = 0;
+    foreach (T item in sequence)
+    {
+        if (i++ % frequency == 0)
+            yield return item;
+    }
+}
+
 public void DoProcessing()
 {
     TraceMessage("Something happened.");
