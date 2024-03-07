9c0255a6ed823b8113a868a8d5844f6093a48929
Ronnie <ronnie94official@gmail.com>
2024-03-07 12:17:49 +0000

    Create code_01.cs

---
+++ cs/code_01.cs
@@ -0,0 +1,52 @@
+private void DrawGraph()
+{
+    const float wxmin = -2;
+    const float wxmax = 2;
+    const float wymin = -2;
+    const float wymax = 2;
+
+    int wid = picGraph.ClientSize.Width;
+    int hgt = picGraph.ClientSize.Height;
+    Bitmap bm = new Bitmap(wid, hgt);
+    using (Graphics gr = Graphics.FromImage(bm))
+    {
+        gr.SmoothingMode = SmoothingMode.AntiAlias;
+
+        RectangleF rect = new RectangleF(
+            wxmin, wymin, (wxmax - wxmin), (wymax - wymin));
+        PointF[] points =
+        {
+            new PointF(0, hgt),
+            new PointF(wid, hgt),
+            new PointF(0, 0),
+        };
+        gr.Transform = new Matrix(rect, points);
+        Matrix inverse = gr.Transform;
+        inverse.Invert();
+
+        // Draw the axes.
+        using (Pen pen = new Pen(Color.Red, 0))
+        {
+            gr.DrawLine(pen, wxmin, 0, wxmax, 0);
+            gr.DrawLine(pen, 0, wymin, 0, wymax);
+        }
+
+        // Plot the function.
+        // Convert X coordinates for each pixel into world coordinates.
+        PointF[] values = new PointF[wid];
+        for (int i = 0; i < wid; i++) values[i].X = i;
+        inverse.TransformPoints(values);
+
+        // Generate Y values.
+        for (int i = 0; i < wid; i++)
+            values[i].Y = F(values[i].X, A, B);
+
+        // Plot.
+        using (Pen pen = new Pen(Color.Black, 0))
+        {
+            gr.DrawLines(pen, values);
+        }
+    }
+
+    picGraph.Image = bm;
+}
