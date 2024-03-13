17124a6ba6f487dc616b9d828a6e664cb3e3e38b
Ronnie <ronnie94official@gmail.com>
2024-03-13 14:38:23 +0000

    Update api.py

--- py/api.py
+++ py/api.py
@@ -1,10 +1,2 @@
 def terminate():
-  """
-  The function `terminate()` returns `True`, indicating that it terminates the
-  execution of the program or procedure.
-
-  Returns:
-      bool: The output of this function is True.
-
-  """
   return True
